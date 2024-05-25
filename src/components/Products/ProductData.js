6827300dc7d4f280135cac54e4f917391b4d0bf5
sisindri <sisindrisingamsetti@gmail.com>
2024-05-25 19:56:23 +0530

    dummy data updated for products 25.05.2024 19:56

--- src/components/Products/ProductData.js
+++ src/components/Products/ProductData.js
@@ -19,481 +19,601 @@ export const productsData = [
     id: 2,
     image: "path/to/image2.jpg",
     name: "Product B",
+    subtitle: "Free Standing, Finish Color - Wenge/White, DIY(Do-It-Yourself)",
+    offerprice: "9,240",
+    originalprice: "12,000",
+    discount: "23",
     buyButton: {
       text: "Buy Now",
       onClick: () => {
         // Function to handle buy button click for Product B
       },
     },
-    price: 2000,
   },
   {
     id: 3,
     image: "path/to/image3.jpg",
     name: "Product C",
+    subtitle: "Free Standing, Finish Color - Wenge/White, DIY(Do-It-Yourself)",
+    offerprice: "9,240",
+    originalprice: "12,000",
+    discount: "23",
     buyButton: {
       text: "Buy Now",
       onClick: () => {
         // Function to handle buy button click for Product C
       },
     },
-    price: 2000,
   },
   {
     id: 4,
     image: "path/to/image4.jpg",
     name: "Product D",
+    subtitle: "Free Standing, Finish Color - Wenge/White, DIY(Do-It-Yourself)",
+    offerprice: "9,240",
+    originalprice: "12,000",
+    discount: "23",
     buyButton: {
       text: "Buy Now",
       onClick: () => {
         // Function to handle buy button click for Product D
       },
     },
-    price: 2000,
   },
   {
     id: 5,
     image: "path/to/image5.jpg",
     name: "Product E",
+    subtitle: "Free Standing, Finish Color - Wenge/White, DIY(Do-It-Yourself)",
+    offerprice: "9,240",
+    originalprice: "12,000",
+    discount: "23",
     buyButton: {
       text: "Buy Now",
       onClick: () => {
         // Function to handle buy button click for Product E
       },
     },
-    price: 2000,
   },
   {
     id: 6,
     image: "path/to/image6.jpg",
     name: "Product F",
+    subtitle: "Free Standing, Finish Color - Wenge/White, DIY(Do-It-Yourself)",
+    offerprice: "9,240",
+    originalprice: "12,000",
+    discount: "23",
     buyButton: {
       text: "Buy Now",
       onClick: () => {
         // Function to handle buy button click for Product F
       },
     },
-    price: 2000,
   },
   {
     id: 7,
     image: "path/to/image7.jpg",
     name: "Product G",
+    subtitle: "Free Standing, Finish Color - Wenge/White, DIY(Do-It-Yourself)",
+    offerprice: "9,240",
+    originalprice: "12,000",
+    discount: "23",
     buyButton: {
       text: "Buy Now",
       onClick: () => {
         // Function to handle buy button click for Product G
       },
     },
-    price: 2000,
   },
   {
     id: 8,
     image: "path/to/image8.jpg",
     name: "Product H",
+    subtitle: "Free Standing, Finish Color - Wenge/White, DIY(Do-It-Yourself)",
+    offerprice: "9,240",
+    originalprice: "12,000",
+    discount: "23",
     buyButton: {
       text: "Buy Now",
       onClick: () => {
         // Function to handle buy button click for Product H
       },
     },
-    price: 2000,
   },
   {
     id: 9,
     image: "path/to/image9.jpg",
     name: "Product I",
+    subtitle: "Free Standing, Finish Color - Wenge/White, DIY(Do-It-Yourself)",
+    offerprice: "9,240",
+    originalprice: "12,000",
+    discount: "23",
     buyButton: {
       text: "Buy Now",
       onClick: () => {
         // Function to handle buy button click for Product I
       },
     },
-    price: 2000,
   },
   {
     id: 10,
     image: "path/to/image10.jpg",
     name: "Product J",
+    subtitle: "Free Standing, Finish Color - Wenge/White, DIY(Do-It-Yourself)",
+    offerprice: "9,240",
+    originalprice: "12,000",
+    discount: "23",
     buyButton: {
       text: "Buy Now",
       onClick: () => {
         // Function to handle buy button click for Product J
       },
     },
-    price: 2000,
   },
   {
     id: 11,
     image: "path/to/image11.jpg",
     name: "Product K",
+    subtitle: "Free Standing, Finish Color - Wenge/White, DIY(Do-It-Yourself)",
+    offerprice: "9,240",
+    originalprice: "12,000",
+    discount: "23",
     buyButton: {
       text: "Buy Now",
       onClick: () => {
         // Function to handle buy button click for Product K
       },
     },
-    price: 2000,
   },
   {
     id: 12,
     image: "path/to/image12.jpg",
     name: "Product L",
+    subtitle: "Free Standing, Finish Color - Wenge/White, DIY(Do-It-Yourself)",
+    offerprice: "9,240",
+    originalprice: "12,000",
+    discount: "23",
     buyButton: {
       text: "Buy Now",
       onClick: () => {
         // Function to handle buy button click for Product L
       },
     },
-    price: 2000,
   },
   {
     id: 13,
     image: "path/to/image13.jpg",
     name: "Product M",
+    subtitle: "Free Standing, Finish Color - Wenge/White, DIY(Do-It-Yourself)",
+    offerprice: "9,240",
+    originalprice: "12,000",
+    discount: "23",
     buyButton: {
       text: "Buy Now",
       onClick: () => {
         // Function to handle buy button click for Product M
       },
     },
-    price: 2000,
   },
   {
     id: 14,
     image: "path/to/image14.jpg",
     name: "Product N",
+    subtitle: "Free Standing, Finish Color - Wenge/White, DIY(Do-It-Yourself)",
+    offerprice: "9,240",
+    originalprice: "12,000",
+    discount: "23",
     buyButton: {
       text: "Buy Now",
       onClick: () => {
         // Function to handle buy button click for Product N
       },
     },
-    price: 2000,
   },
   {
     id: 15,
     image: "path/to/image15.jpg",
     name: "Product O",
+    subtitle: "Free Standing, Finish Color - Wenge/White, DIY(Do-It-Yourself)",
+    offerprice: "9,240",
+    originalprice: "12,000",
+    discount: "23",
     buyButton: {
       text: "Buy Now",
       onClick: () => {
         // Function to handle buy button click for Product O
       },
     },
-    price: 2000,
   },
   {
     id: 16,
     image: "path/to/image15.jpg",
     name: "Product ppp",
+    subtitle: "Free Standing, Finish Color - Wenge/White, DIY(Do-It-Yourself)",
+    offerprice: "9,240",
+    originalprice: "12,000",
+    discount: "23",
     buyButton: {
       text: "Buy Now",
       onClick: () => {
         // Function to handle buy button click for Product O
       },
     },
-    price: 2000,
   },
   {
     id: 17,
     image: "path/to/image15.jpg",
     name: "Product uuu",
+    subtitle: "Free Standing, Finish Color - Wenge/White, DIY(Do-It-Yourself)",
+    offerprice: "9,240",
+    originalprice: "12,000",
+    discount: "23",
     buyButton: {
       text: "Buy Now",
       onClick: () => {
         // Function to handle buy button click for Product O
       },
     },
-    price: 2000,
   },
   {
     id: 18,
     image: "path/to/image15.jpg",
     name: "Product yyy",
+    subtitle: "Free Standing, Finish Color - Wenge/White, DIY(Do-It-Yourself)",
+    offerprice: "9,240",
+    originalprice: "12,000",
+    discount: "23",
     buyButton: {
       text: "Buy Now",
       onClick: () => {
         // Function to handle buy button click for Product O
       },
     },
-    price: 2000,
   },
   {
     id: 19,
     image: "path/to/image15.jpg",
     name: "Product flg",
+    subtitle: "Free Standing, Finish Color - Wenge/White, DIY(Do-It-Yourself)",
+    offerprice: "9,240",
+    originalprice: "12,000",
+    discount: "23",
     buyButton: {
       text: "Buy Now",
       onClick: () => {
         // Function to handle buy button click for Product O
       },
     },
-    price: 2000,
   },
   {
     id: 20,
     image: "path/to/image15.jpg",
     name: "Product dfklg",
+    subtitle: "Free Standing, Finish Color - Wenge/White, DIY(Do-It-Yourself)",
+    offerprice: "9,240",
+    originalprice: "12,000",
+    discount: "23",
     buyButton: {
       text: "Buy Now",
       onClick: () => {
         // Function to handle buy button click for Product O
       },
     },
-    price: 2000,
   },
   {
     id: 21,
     image: "path/to/image15.jpg",
     name: "Product vdk",
+    subtitle: "Free Standing, Finish Color - Wenge/White, DIY(Do-It-Yourself)",
+    offerprice: "9,240",
+    originalprice: "12,000",
+    discount: "23",
     buyButton: {
       text: "Buy Now",
       onClick: () => {
         // Function to handle buy button click for Product O
       },
     },
-    price: 2000,
   },
   {
     id: 22,
     image: "path/to/image15.jpg",
     name: "Product adv",
+    subtitle: "Free Standing, Finish Color - Wenge/White, DIY(Do-It-Yourself)",
+    offerprice: "9,240",
+    originalprice: "12,000",
+    discount: "23",
     buyButton: {
       text: "Buy Now",
       onClick: () => {
         // Function to handle buy button click for Product O
       },
     },
-    price: 2000,
   },
   {
     id: 23,
     image: "path/to/image15.jpg",
     name: "Product qwwqe",
+    subtitle: "Free Standing, Finish Color - Wenge/White, DIY(Do-It-Yourself)",
+    offerprice: "9,240",
+    originalprice: "12,000",
+    discount: "23",
     buyButton: {
       text: "Buy Now",
       onClick: () => {
         // Function to handle buy button click for Product O
       },
     },
-    price: 2000,
   },
   {
     id: 24,
     image: "path/to/image15.jpg",
     name: "Product ppoyh",
+    subtitle: "Free Standing, Finish Color - Wenge/White, DIY(Do-It-Yourself)",
+    offerprice: "9,240",
+    originalprice: "12,000",
+    discount: "23",
     buyButton: {
       text: "Buy Now",
       onClick: () => {
         // Function to handle buy button click for Product O
       },
     },
-    price: 2000,
   },
   {
     id: 25,
     image: "path/to/image15.jpg",
     name: "Product wrui",
+    subtitle: "Free Standing, Finish Color - Wenge/White, DIY(Do-It-Yourself)",
+    offerprice: "9,240",
+    originalprice: "12,000",
+    discount: "23",
     buyButton: {
       text: "Buy Now",
       onClick: () => {
         // Function to handle buy button click for Product O
       },
     },
-    price: 2000,
   },
   {
     id: 26,
     image: "path/to/image15.jpg",
     name: "Product spofp",
+    subtitle: "Free Standing, Finish Color - Wenge/White, DIY(Do-It-Yourself)",
+    offerprice: "9,240",
+    originalprice: "12,000",
+    discount: "23",
     buyButton: {
       text: "Buy Now",
       onClick: () => {
         // Function to handle buy button click for Product O
       },
     },
-    price: 2000,
   },
   {
     id: 27,
     image: "path/to/image15.jpg",
     name: "Product soi",
+    subtitle: "Free Standing, Finish Color - Wenge/White, DIY(Do-It-Yourself)",
+    offerprice: "9,240",
+    originalprice: "12,000",
+    discount: "23",
     buyButton: {
       text: "Buy Now",
       onClick: () => {
         // Function to handle buy button click for Product O
       },
     },
-    price: 2000,
   },
   {
     id: 28,
     image: "path/to/image15.jpg",
     name: "Product ngh",
+    subtitle: "Free Standing, Finish Color - Wenge/White, DIY(Do-It-Yourself)",
+    offerprice: "9,240",
+    originalprice: "12,000",
+    discount: "23",
     buyButton: {
       text: "Buy Now",
       onClick: () => {
         // Function to handle buy button click for Product O
       },
     },
-    price: 2000,
   },
   {
     id: 29,
     image: "path/to/image15.jpg",
     name: "Product ngh",
+    subtitle: "Free Standing, Finish Color - Wenge/White, DIY(Do-It-Yourself)",
+    offerprice: "9,240",
+    originalprice: "12,000",
+    discount: "23",
     buyButton: {
       text: "Buy Now",
       onClick: () => {
         // Function to handle buy button click for Product O
       },
     },
-    price: 2000,
   },
   {
     id: 30,
     image: "path/to/image15.jpg",
     name: "Product zsd",
+    subtitle: "Free Standing, Finish Color - Wenge/White, DIY(Do-It-Yourself)",
+    offerprice: "9,240",
+    originalprice: "12,000",
+    discount: "23",
     buyButton: {
       text: "Buy Now",
       onClick: () => {
         // Function to handle buy button click for Product O
       },
     },
-    price: 2000,
   },
   {
     id: 31,
     image: "path/to/image15.jpg",
     name: "Product nv",
+    subtitle: "Free Standing, Finish Color - Wenge/White, DIY(Do-It-Yourself)",
+    offerprice: "9,240",
+    originalprice: "12,000",
+    discount: "23",
     buyButton: {
       text: "Buy Now",
       onClick: () => {
         // Function to handle buy button click for Product O
       },
     },
-    price: 2000,
   },
   {
     id: 32,
     image: "path/to/image15.jpg",
     name: "Product erg",
+    subtitle: "Free Standing, Finish Color - Wenge/White, DIY(Do-It-Yourself)",
+    offerprice: "9,240",
+    originalprice: "12,000",
+    discount: "23",
     buyButton: {
       text: "Buy Now",
       onClick: () => {
         // Function to handle buy button click for Product O
       },
     },
-    price: 2000,
   },
   {
     id: 33,
     image: "path/to/image15.jpg",
     name: "Product ergie",
+    subtitle: "Free Standing, Finish Color - Wenge/White, DIY(Do-It-Yourself)",
+    offerprice: "9,240",
+    originalprice: "12,000",
+    discount: "23",
     buyButton: {
       text: "Buy Now",
       onClick: () => {
         // Function to handle buy button click for Product O
       },
     },
-    price: 2000,
   },
   {
     id: 34,
     image: "path/to/image15.jpg",
     name: "Product guir",
+    subtitle: "Free Standing, Finish Color - Wenge/White, DIY(Do-It-Yourself)",
+    offerprice: "9,240",
+    originalprice: "12,000",
+    discount: "23",
     buyButton: {
       text: "Buy Now",
       onClick: () => {
         // Function to handle buy button click for Product O
       },
     },
-    price: 2000,
   },
   {
     id: 35,
     image: "path/to/image15.jpg",
     name: "Product per",
+    subtitle: "Free Standing, Finish Color - Wenge/White, DIY(Do-It-Yourself)",
+    offerprice: "9,240",
+    originalprice: "12,000",
+    discount: "23",
     buyButton: {
       text: "Buy Now",
       onClick: () => {
         // Function to handle buy button click for Product O
       },
     },
-    price: 2000,
   },
   {
     id: 36,
     image: "path/to/image15.jpg",
     name: "Product eger",
+    subtitle: "Free Standing, Finish Color - Wenge/White, DIY(Do-It-Yourself)",
+    offerprice: "9,240",
+    originalprice: "12,000",
+    discount: "23",
     buyButton: {
       text: "Buy Now",
       onClick: () => {
         // Function to handle buy button click for Product O
       },
     },
-    price: 2000,
   },
   {
     id: 37,
     image: "path/to/image15.jpg",
     name: "Product ferg",
+    subtitle: "Free Standing, Finish Color - Wenge/White, DIY(Do-It-Yourself)",
+    offerprice: "9,240",
+    originalprice: "12,000",
+    discount: "23",
     buyButton: {
       text: "Buy Now",
       onClick: () => {
         // Function to handle buy button click for Product O
       },
     },
-    price: 2000,
   },
   {
     id: 38,
     image: "path/to/image15.jpg",
     name: "Product erfiu",
+    subtitle: "Free Standing, Finish Color - Wenge/White, DIY(Do-It-Yourself)",
+    offerprice: "9,240",
+    originalprice: "12,000",
+    discount: "23",
     buyButton: {
       text: "Buy Now",
       onClick: () => {
         // Function to handle buy button click for Product O
       },
     },
-    price: 2000,
   },
   {
     id: 39,
     image: "path/to/image15.jpg",
     name: "Product egfe",
+    subtitle: "Free Standing, Finish Color - Wenge/White, DIY(Do-It-Yourself)",
+    offerprice: "9,240",
+    originalprice: "12,000",
+    discount: "23",
     buyButton: {
       text: "Buy Now",
       onClick: () => {
         // Function to handle buy button click for Product O
       },
     },
-    price: 2000,
   },
   {
     id: 40,
     image: "path/to/image15.jpg",
     name: "Product hgf",
+    subtitle: "Free Standing, Finish Color - Wenge/White, DIY(Do-It-Yourself)",
+    offerprice: "9,240",
+    originalprice: "12,000",
+    discount: "23",
     buyButton: {
       text: "Buy Now",
       onClick: () => {
         // Function to handle buy button click for Product O
       },
     },
-    price: 2000,
   },
   {
     id: 41,
     image: "path/to/image15.jpg",
     name: "Product 69",
+    subtitle: "Free Standing, Finish Color - Wenge/White, DIY(Do-It-Yourself)",
+    offerprice: "9,240",
+    originalprice: "12,000",
+    discount: "23",
     buyButton: {
       text: "Buy Now",
       onClick: () => {
         // Function to handle buy button click for Product O
       },
     },
-    price: 2000,
   },
 ];
 productsData.forEach((product, index) => {
